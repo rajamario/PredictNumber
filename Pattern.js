c7a9c8e44108882b3fd3410339f9726568d9c8ac
RajaMario <rajah.m@gmail.com>
2022-04-24 18:14:12 -0500

    Changes for style

--- Pattern.js
+++ Pattern.js
@@ -1,5 +1,7 @@
 var numbers = new Array(0, 2, 14, 35, 23, 4, 16, 33, 21, 6, 18, 31, 19, 8, 12, 29, 25, 10, 27, 88, 1, 13, 36, 24, 3, 15, 34, 22, 5, 17, 32, 20, 7, 11, 30, 26, 9, 28);
 var todayPattern = new Array(88, 32, 34, 23, 19, 15, 18, 36, 24, 27, 5, 35, 6, 19, 25, 36, 25, 29, 5, 14, 17, 33);
+var red = new Array(1,3,36,34,5,32,7,30,9,14,23,16,21,18,19,12,25,27);
+var black = new Array(2,35,4,33,6,31,8,29,10,13,24,15,22,17,20,11,26,28);
 var db = [
 	//new Array(16, 10, 28, 3, 6, 35, 16, 7, 16, 35, 25, 11, 2, 88, 30, 3, 26, 15, 12, 13, 16, 14, 34),
 	//new Array(6, 88, 19, 24, 26, 34, 2, 6, 32, 20, 31, 8, 33, 34, 29, 6, 5, 34, 19, 1, 20, 12, 30)
@@ -10,13 +12,13 @@ db.push(ongoingSpins);
 function showSlots() {
 	var slot = "<table style=\"position:relative;\"><tr>";
 	for (var i = 1; i <= 36; i++) {
-		slot += "<td align=\"center\"><input type=\"button\" onclick=\"addToOngoingSpin(this)\" value=\"" + i + "\" \/><\/td>";
+		slot += "<td align=\"center\"><input type=\"button\" style=\"width:50px\" onclick=\"addToOngoingSpin(this)\" value=\"" + i + "\" \/><\/td>";
 		if (i % 6 == 0) {
 			slot+= "<\/tr><tr>";
 		}
 	}
-	slot += "<tr><td colspan=\"3\" align=\"right\"><input type=\"button\" onclick=\"addToOngoingSpin(this)\" value=\"0\" \/><\/td>";
-	slot += "<td colspan=\"3\" \"left\"><input type=\"button\" onclick=\"addToOngoingSpin(this)\" value=\"88\" \/><\/td><\/tr>";
+	slot += "<tr><td colspan=\"3\" align=\"right\"><input type=\"button\" style=\"width:50px\" onclick=\"addToOngoingSpin(this)\" value=\"0\" \/><\/td>";
+	slot += "<td colspan=\"3\" \"left\"><input type=\"button\" style=\"width:50px\" onclick=\"addToOngoingSpin(this)\" value=\"88\" \/><\/td><\/tr>";
 	slot += "<\/tr><\/table>";
 	$("#slots").html(slot);
 }
@@ -80,16 +82,13 @@ function showSpinAnalysis() {
 			var ret = getPositions(list[i], list[i - 1]);
 			if (ret != -1) {
 				moved.push(ret);
-				//moved = getPositions(list[i], list[i - 1]);
-				// console.log("from:" + actual.get(i) + " :: To: " + actual.get(i - 1) +
-				//print= print + moved + "-";
 			}
 		}
 		//print+="\n<br>";
 	});
 	var patternButton ="";
 	moved.forEach(function(v) {
-		patternButton += "<input type=\"button\" onclick=\"planNextMove(this)\" value=\""+ v +"\" \/>&nbsp;&nbsp;";
+		patternButton += "<input type=\"button\" style=\"width:50px\" onclick=\"planNextMove(this)\" value=\""+ v +"\" \/>&nbsp;&nbsp;";
 		print += v + " - ";
 	});
 	console.log(print);
@@ -156,4 +155,10 @@ function locateSlots(formation, moves) {
 	}
 	//console.log("next:" + positions);
 	return positions;
+}
+
+function checkReload(){
+	if(confirm("Spin Details will be lost. Do you want to refresh ??")){
+		window.location.reload();
+	}	
 }
